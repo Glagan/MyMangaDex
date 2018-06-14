06982160124eb070d6f859b28c3504b1eb82ce00
Glagan <Glagan@users.noreply.github.com>
2018-06-14 22:31:39 +0200

    - Better readme - Added storage permission - Removed useless lines in MyMangaDex object - Separated each pages in functions - Fix page detection - Store and search local storage for mal_id<=>mangadex_id pair to avoid searching and for chapter page

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Chapter page
 
 Stored data
 * Last opened chapter for each manga (manga id, manga name, chapter url, chapter name)
-* MAL url for a manga on mangadex (mal url, mangadex url)
+* MAL url for a manga on mangadex (mal url, mangadex url) with a key mangadex id and the value is the mal id
 
 ---
 
@@ -31,30 +31,46 @@ Required header to POST
 }
 ```
 
+Colors used:
+* Last open:                       rebeccapurple
+* Actual last read chapter on mal: cadetblue
+
 ---
 
 Detect chapter number:
 * Ch. [number]
+Optionnal sub chapter (the floating point)
+* Ch. [number].[sub_chapter]
 And the volume number (optionnal) on
 * Vol. [number]
 
 ---
 
-Colors:
-* Last open: rebeccapurple
-* Actual last read chapter on mal: cadetblue
+##Manga Page
+1. check if user is logged in
+  * abort if not logged in
+2. get manga name and mangadex_id
+3. check if there is a entry for this manga in the local storage
+  * if there is an entry, add the mal id to the MyMangaDex object
+  * if there is no entry, check if there is a mal link
+    * if there is a mal link, add it to the MyMangaDex object and add the entry is the local storage
+    * if there is no mal link, add the link entry form
 
----
-
+#Change log
 Before release done:
 * detect which page is the current one
 * detect if user is logged in
-* TODO: check only every x minutes or so but check when sending data
 * fetch last read chapter on mal and display it
 * highlight last read chapter
 * detect each chapters, with optionnal volume and "sub" chapter (eg: 18[.2])
 
-Todo:
+* Better readme
+* separated each pages in functions for clarity
+* Check if there is a mal id stored to avoid looking for it
+* Store the mal url in local storage
+
+##TODO
+* check only every x minutes or so but check when sending data
 * Follow page:
   * Highlight if last_read
 * Manga page:
@@ -65,3 +81,17 @@ Todo:
 * chapter page:
   * edit last read on mal and last opened
   * add manga to reading list if not in
+
+##Legacy code
+Optionnal way to fetch manga and chapter info on chapter page:
+```javascript
+MyMangaDex.manga_name = document.getElementsByClassName("panel-title")[0].textContent;
+
+var chapter_select = document.querySelector("button[data-id='jump_chapter']");
+var volume_and_chapter = /(Volume\s(\d+)|).*(Chapter\s(\d+))\.*(\d+)*/.exec(chapter_select.title);
+MyMangaDex.current_chapter = {
+  volume: volume_and_chapter[2],
+  chapter: volume_and_chapter[4],
+  sub_chapter: volume_and_chapter[5]
+}
+```
--- mymangadex.js
+++ mymangadex.js
@@ -2,140 +2,38 @@
 
 window.MyMangaDex = {
     url: "",
-    page: "",
+    manga_name: "",
+    mangadex_id: 0,
     mal_id: 0,
     mal_url: "",
-    manga_name: "",
-    volume: "",
-    chapter: "",
     last_read: 0,
     last_open: 0,
-    chapters: []
+    chapters: [],
+    current_chapter: {},
 };
 
 // FUNCTIONS
 
+// https://stackoverflow.com/a/34491287/7794671
+function isEmpty(obj) {
+   for (var x in obj) { return false; }
+   return true;
+}
+
 function start(logged_in) {
+    // If user isn't logged in, can't do anything
     if (logged_in) {
         MyMangaDex.url = window.location.href;
 
-        if (MyMangaDex.url.indexOf("/follows") > -1) {
+        if (MyMangaDex.url.indexOf("org/follows") > -1) {
             console.log("Follow page");
-        } else if (MyMangaDex.url.indexOf("/manga")) {
+            follow_page();
+        } else if (MyMangaDex.url.indexOf("org/manga") > -1) {
             console.log("Manga page");
-            MyMangaDex.manga_name = document.getElementsByClassName("panel-title")[0].textContent;
-            MyMangaDex.mal_url = document.querySelector("img[src='/images/misc/mal.png'");
-
-            // Check if there is a MAL Link
-            if (MyMangaDex.mal_url !== null) {
-                // Finish getting the mal link
-                MyMangaDex.mal_url = MyMangaDex.mal_url.nextElementSibling;
-
-                // Get MAL id of the manga from the mal link
-                MyMangaDex.mal_id = /.+\/(\d+)/.exec(MyMangaDex.mal_url)[1];
-
-                // Fetch the edit page of the manga
-                // Overkill until api come to life
-                fetch("https://myanimelist.net/ownlist/manga/" + MyMangaDex.mal_id + "/edit", {
-                    method: 'GET',
-                    redirect: 'follow',
-                    credentials: 'include'
-                }).then((data) => {
-                    data.text().then((text) => {
-                        // Scan the fetch page for the last read chapter
-                        MyMangaDex.last_read = /add_manga_num_read_chapters.+value="(\d+)*"/.exec(text);
-
-                        // init for both if and else
-                        var parent_node = document.getElementsByClassName("table table-condensed")[0].firstElementChild;
-                        var chapters_row = document.createElement("tr");
-                        var chapters_column_header = document.createElement("th");
-                        chapters_column_header.textContent = "MAL Progress:";
-                        var chapters_column_content = document.createElement("td");
-
-                        // Check if the manga is already in the reading list
-                        if (MyMangaDex.last_read !== null) {
-                            // Finsih processing the last_read
-                            MyMangaDex.last_read = MyMangaDex.last_read[1];
-
-                            // Add the current chapter on the page
-                            chapters_column_content.textContent = "Chapter " + MyMangaDex.last_read + " ";
-                            var chapters_column_content_edit = document.createElement("a");
-                            chapters_column_content_edit.className = "btn btn-default";
-                            chapters_column_content_edit.href = "https://myanimelist.net/ownlist/manga/" + MyMangaDex.mal_id + "/edit";
-                            chapters_column_content_edit.textContent = "Edit in MAL";
-                            // Append nodes
-                            chapters_column_content.appendChild(chapters_column_content_edit);
-
-                            // Highlight last read chapter
-                            for (var chapter of MyMangaDex.chapters) {
-                                if (chapter.chapter == MyMangaDex.last_read) {
-                                    chapter.parent_node.style.backgroundColor = "cadetblue";
-                                }
-                            }
-                        // Else add a button to add it
-                        } else {
-                            // Add a "Add to reading list" button
-                            var chapters_column_content_add = document.createElement("button");
-                            chapters_column_content_add.className = "btn btn-default";
-                            chapters_column_content_add.textContent = "Add to MAL reading list";
-                            chapters_column_content.appendChild(chapters_column_content_add);
-                        }
-
-                        // Append nodes to the table to display
-                        chapters_row.appendChild(chapters_column_header);
-                        chapters_row.appendChild(chapters_column_content);
-                        parent_node.insertBefore(chapters_row, parent_node.lastElementChild);
-                    });
-                }).catch((error) => {
-                    console.error(error);
-                });
-
-                // Chapters list displayed
-                var main_table = document.getElementsByClassName("table table-striped table-hover table-condensed")[0];
-                var main_chapter_table = main_table.querySelector("tbody");
-
-                // Get the name of each "chapters" in the list
-                for (var element of main_chapter_table.children) {
-                    var name = element.children[1].firstChild.textContent;
-                    var volume_and_chapter = /(Vol\.\s(\d+)|).*(Ch\.\s(\d+))\.*(\d+)*/.exec(name);
-                    MyMangaDex.chapters.push({
-                        name: name,
-                        parent_node: element,
-                        volume: (volume_and_chapter[2] === undefined) ? 0 : volume_and_chapter[2],
-                        chapter: volume_and_chapter[4],
-                        sub_chapter: volume_and_chapter[5]
-                    });
-                }
-            // If there is no MAL link, you can link one
-            } else {
-                var parent_node = document.getElementsByClassName("table table-condensed")[0].firstElementChild;
-                var add_mal_link_row = document.createElement("tr");
-                var add_mal_link_column_header = document.createElement("th");
-                add_mal_link_column_header.textContent = "MAL link:";
-                var add_mal_link_column_content = document.createElement("td");
-                var add_mal_link_column_content_edit = document.createElement("input");
-                add_mal_link_column_content_edit.id = "mymangadex-mal-link-input";
-                add_mal_link_column_content_edit.className = "form-control";
-                // Style the input since the form-control style is fucked
-                add_mal_link_column_content_edit.style.display = "inline-block";
-                add_mal_link_column_content_edit.style.width = "auto";
-                add_mal_link_column_content_edit.style.verticalAlign = "middle";
-                add_mal_link_column_content_edit.type = "text";
-                add_mal_link_column_content_edit.size = 40;
-                add_mal_link_column_content_edit.placeholder = "eg: https://myanimelist.net/manga/103939";
-                var add_mal_link_column_content_send = document.createElement("button");
-                add_mal_link_column_content_send.className = "btn btn-default"
-                add_mal_link_column_content_send.type = "submit";
-                add_mal_link_column_content_send.textContent = "Send";
-                add_mal_link_column_content.appendChild(add_mal_link_column_content_edit);
-                add_mal_link_column_content.appendChild(add_mal_link_column_content_send);
-                add_mal_link_row.appendChild(add_mal_link_column_header);
-                add_mal_link_row.appendChild(add_mal_link_column_content);
-                parent_node.insertBefore(add_mal_link_row, parent_node.lastElementChild);
-            }
-        // If there isn't any MAL Link, create text box to enter one
-        } else if (MyMangaDex.url.indexOf("/chapter")) {
+            manga_page();
+        } else if (MyMangaDex.url.indexOf("org/chapter") > -1) {
             console.log("Chapter page");
+            chapter_page();
         }
     } else {
         console.error("Not logged in MyAnimeList, or there is a problem accessing the login page.");
@@ -153,8 +51,193 @@ function is_logged_in() {
     // Redirected => logged in
 }
 
+function insert_mal_link_form() {
+    var parent_node = document.getElementsByClassName("table table-condensed")[0].firstElementChild;
+    var add_mal_link_row = document.createElement("tr");
+    var add_mal_link_column_header = document.createElement("th");
+    add_mal_link_column_header.textContent = "MAL link:";
+    var add_mal_link_column_content = document.createElement("td");
+    var add_mal_link_column_content_edit = document.createElement("input");
+    add_mal_link_column_content_edit.id = "mymangadex-mal-link-input";
+    add_mal_link_column_content_edit.className = "form-control";
+    // Style the input since the form-control style is fucked
+    add_mal_link_column_content_edit.style.display = "inline-block";
+    add_mal_link_column_content_edit.style.width = "auto";
+    add_mal_link_column_content_edit.style.verticalAlign = "middle";
+    add_mal_link_column_content_edit.type = "text";
+    add_mal_link_column_content_edit.size = 40;
+    add_mal_link_column_content_edit.placeholder = "eg: https://myanimelist.net/manga/103939";
+    var add_mal_link_column_content_send = document.createElement("button");
+    add_mal_link_column_content_send.className = "btn btn-default"
+    add_mal_link_column_content_send.type = "submit";
+    add_mal_link_column_content_send.textContent = "Send";
+    add_mal_link_column_content.appendChild(add_mal_link_column_content_edit);
+    add_mal_link_column_content.appendChild(add_mal_link_column_content_send);
+    add_mal_link_row.appendChild(add_mal_link_column_header);
+    add_mal_link_row.appendChild(add_mal_link_column_content);
+    parent_node.insertBefore(add_mal_link_row, parent_node.lastElementChild);
+}
+
+function follow_page() {
+}
+
+/**
+ * Manga page where there is the description and a list of the last 100 chapters of a manga
+ * Optionnal MAL url with a mal icon
+ */
+function manga_page() {
+    MyMangaDex.manga_name = document.getElementsByClassName("panel-title")[0].textContent.trim();
+    MyMangaDex.mangadex_id = /.+manga\/(\d+)/.exec(MyMangaDex.url)[1];
+
+    // Fetch the manga information from the local storage
+    browser.storage.local.get(MyMangaDex.mangadex_id)
+    .then((data) => {
+        var has_a_mal_link = true;
+
+        // If there is no entry try to find it
+        if (isEmpty(data)) {
+            // Search the icon, find the link
+            MyMangaDex.mal_url = document.querySelector("img[src='/images/misc/mal.png'");
+
+            if (MyMangaDex.mal_url !== null) {
+                // Finish getting the mal link
+                MyMangaDex.mal_url = MyMangaDex.mal_url.nextElementSibling.href;
+                // Get MAL id of the manga from the mal link
+                MyMangaDex.mal_id = /.+\/(\d+)/.exec(MyMangaDex.mal_url)[1];
+
+                // Add the entry to the local storage, to avoid searching again next time
+                browser.storage.local.set({
+                    [MyMangaDex.mangadex_id]: MyMangaDex.mal_id
+                });
+            // If there is no MAL link, you can link one
+            } else {
+                has_a_mal_link = false;
+                insert_mal_link_form();
+            }
+        } else {
+            MyMangaDex.mal_id = data[MyMangaDex.mangadex_id];
+        }
+
+        // If there is a existing mal link
+        if (has_a_mal_link) {
+            // Fetch the edit page of the manga
+            // Overkill until api come to life
+            fetch("https://myanimelist.net/ownlist/manga/" + MyMangaDex.mal_id + "/edit", {
+                method: 'GET',
+                redirect: 'follow',
+                credentials: 'include'
+            }).then((data) => {
+                data.text().then((text) => {
+                    // Scan the fetch page for the last read chapter
+                    MyMangaDex.last_read = /add_manga_num_read_chapters.+value="(\d+)*"/.exec(text);
+
+                    // init for both if and else
+                    var parent_node = document.getElementsByClassName("table table-condensed")[0].firstElementChild;
+                    var chapters_row = document.createElement("tr");
+                    var chapters_column_header = document.createElement("th");
+                    chapters_column_header.textContent = "MAL Progress:";
+                    var chapters_column_content = document.createElement("td");
+
+                    // Check if the manga is already in the reading list
+                    if (MyMangaDex.last_read !== null) {
+                        // Finsih processing the last_read
+                        MyMangaDex.last_read = MyMangaDex.last_read[1];
+
+                        // Add the current chapter on the page
+                        chapters_column_content.textContent = "Chapter " + MyMangaDex.last_read + " ";
+                        var chapters_column_content_edit = document.createElement("a");
+                        chapters_column_content_edit.className = "btn btn-default";
+                        chapters_column_content_edit.href = "https://myanimelist.net/ownlist/manga/" + MyMangaDex.mal_id + "/edit";
+                        chapters_column_content_edit.textContent = "Edit in MAL";
+                        // Append nodes
+                        chapters_column_content.appendChild(chapters_column_content_edit);
+
+                        // Highlight last read chapter
+                        for (var chapter of MyMangaDex.chapters) {
+                            if (chapter.chapter == MyMangaDex.last_read) {
+                                chapter.parent_node.style.backgroundColor = "cadetblue";
+                            }
+                        }
+                        // Else add a button to add it
+                    } else {
+                        // Add a "Add to reading list" button
+                        var chapters_column_content_add = document.createElement("button");
+                        chapters_column_content_add.className = "btn btn-default";
+                        chapters_column_content_add.textContent = "Add to MAL reading list";
+                        chapters_column_content.appendChild(chapters_column_content_add);
+                    }
+
+                    // Append nodes to the table to display
+                    chapters_row.appendChild(chapters_column_header);
+                    chapters_row.appendChild(chapters_column_content);
+                    parent_node.insertBefore(chapters_row, parent_node.lastElementChild);
+                });
+            }).catch((error) => {
+                console.error(error);
+            });
+
+            // Chapters list displayed
+            var main_table = document.getElementsByClassName("table table-striped table-hover table-condensed")[0];
+            var main_chapter_table = main_table.querySelector("tbody");
+
+            // Get the name of each "chapters" in the list
+            for (var element of main_chapter_table.children) {
+                var name = element.children[1].firstChild.textContent;
+                var volume_and_chapter = /(Vol\.\s(\d+)|).*(Ch\.\s(\d+))\.*(\d+)*/.exec(name);
+                MyMangaDex.chapters.push({
+                    name: name,
+                    parent_node: element,
+                    volume: volume_and_chapter[2],
+                    chapter: volume_and_chapter[4],
+                    sub_chapter: volume_and_chapter[5]
+                });
+            }
+        } else {
+            console.log("No MAL link avaible, can't do anything, try to add one if it exist.");
+        }
+
+        // For Devlopment
+        console.log(MyMangaDex);
+        browser.storage.local.get().then(data => console.log(data));
+        // End
+    }, (error) => {
+        console.error("Error fetching data from local storage.", error);
+    });
+}
+
+/**
+ * Chapter page
+ * The volume and chapter number is located on the selector
+ * The MAL URL is fetched from the local database, if there isn't an entry, we look at the manga page, and if there isn't, no more option
+ * All of this is done in the background when the page ended loading, we're not in a hurry anyway
+ */
+function chapter_page() {
+    // Parse the script tag with the info of the chapters and manga inside
+    var manga_info = JSON.parse(document.querySelector("script[data-type='chapter']").textContent);
+    MyMangaDex.manga_name = manga_info.manga_title;
+    MyMangaDex.mangadex_id = manga_info.manga_id;
+
+    // Get MAL Url from the database
+    //MyMangaDex.mal_url = document.querySelector("img[src='/images/misc/mal.png'");
+
+    // Fetch current chapter
+    for (var chapter of manga_info.other_chapters) {
+        if (chapter.id == manga_info.chapter_id) {
+            var volume_and_chapter = /(Volume\s(\d+)|).*(Chapter\s(\d+))\.*(\d+)*/.exec(chapter.name);
+            MyMangaDex.current_chapter = {
+                volume: volume_and_chapter[2],
+                chapter: volume_and_chapter[4],
+                sub_chapter: volume_and_chapter[5]
+            };
+            break;
+        }
+    }
+    console.log(MyMangaDex);
+}
+
 // START HERE
 
+// Check if is logged in and then start the script
 is_logged_in()
 .then((data) => {
     start(data.redirected);
