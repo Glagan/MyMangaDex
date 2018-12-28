0a4d5e9517aa0e836193870a62fd3e45d944455b
Glagan <Glagan@users.noreply.github.com>
2018-12-28 12:24:12 +0100

    Updated MyAnimeList image with a reduced size Added MMD logo and a crossed version for notifications

--- scripts/myMangaDex.js
+++ scripts/myMangaDex.js
@@ -14,7 +14,9 @@ class MyMangaDex {
             currentChapter: {chapter: 0, volume: 0}
         };
         this.fetched = false;
-        this.myAnimeListImage = "https://i.imgur.com/oMV2BJt.png";
+        this.myAnimeListImage = "https://ramune.nikurasu.org/mymangadex/myanimelist.png";
+        this.mmdImage = "https://ramune.nikurasu.org/mymangadex/logo.png";
+        this.mmdCrossedImage = "https://ramune.nikurasu.org/mymangadex/no-logo.png";
     }
 
     async start() {
@@ -659,7 +661,7 @@ class MyMangaDex {
         let data = await storageGet(this.manga.mangaDexId);
         // If there is no entry for mal link
         if (data === undefined) {
-            this.notification(NOTIFY.INFO, "No MyAnimeList ID in storage", "Searching on the manga page of " + this.manga.name + " to find a MyAnimeList id.");
+            this.notification(NOTIFY.INFO, "No MyAnimeList ID in storage", "Searching on the manga page of " + this.manga.name + " to find a MyAnimeList id.", this.mmdImage);
 
             // Fetch it from mangadex manga page
             try {
@@ -672,13 +674,13 @@ class MyMangaDex {
                 let myAnimeListURL = /<a.+href='(.+)'>MyAnimeList<\/a>/.exec(text);
                 // If regex is empty, there is no mal link, can't do anything
                 if (myAnimeListURL === null) {
-                    this.notification(NOTIFY.ERROR, "No MyAnimeList id found", "You will need to go on the manga page if one is added.\nLast open chapters are still saved.", undefined, true);
+                    this.notification(NOTIFY.ERROR, "No MyAnimeList id found", "You will need to go on the manga page if one is added.\nLast open chapters are still saved.", this.mmdCrossedImage, true);
                 } else {
                     // If there is a mal link, add it and save it in local storage
                     this.manga.myAnimeListId = parseInt(/.+\/(\d+)/.exec(myAnimeListURL[1])[1]);
                 }
             } catch (error) {
-                this.notification(NOTIFY.ERROR, "Error fetching MangaDex title page");
+                this.notification(NOTIFY.ERROR, "Error fetching MangaDex title page", undefined, this.mmdCrossedImage);
             }
         } else {
             // Get the mal id from the local storage
