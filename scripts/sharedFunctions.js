d8fe0e07248d5efc648afc6571f79687e213ab2a
Constantin Piber <cp.piber@gmail.com>
2020-04-18 08:45:37 +0200

    Use lastMangaDexChapter in lastMyAnimeListChapter
    On default, use lastMangaDexChapter (if not on reading list or some
    other error), fixes #39

--- scripts/sharedFunctions.js
+++ scripts/sharedFunctions.js
@@ -308,7 +308,7 @@ function processMyAnimeListResponse(manga, text) {
 	manga.ask_to_discuss = (manga.ask_to_discuss === null) ? 0 : parseInt(manga.ask_to_discuss[1]);
 	// Last read chapter
 	manga.lastMyAnimeListChapter = /add_manga_num_read_chapters.+value="(\d+)?"/.exec(text);
-	manga.lastMyAnimeListChapter = (manga.lastMyAnimeListChapter === null) ? -1 : parseInt(manga.lastMyAnimeListChapter[1]);
+	manga.lastMyAnimeListChapter = (manga.lastMyAnimeListChapter === null) ? manga.lastMangaDexChapter : parseInt(manga.lastMyAnimeListChapter[1]);
 	// Total times re-read
 	manga.total_reread = /add_manga_num_read_times.+value="(\d+)?"/.exec(text);
 	manga.total_reread = (manga.total_reread === null) ? 0 : parseInt(manga.total_reread[1]);
